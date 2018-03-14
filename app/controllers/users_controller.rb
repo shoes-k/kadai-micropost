e81afb4858122f5bddc79e7f53fbf45fe5e7159b
shusuke.asami <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2018-03-14 10:36:21 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @users = User.all.page(params[:page])
   end
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def create
-        @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
@@ -25,10 +26,22 @@ class UsersController < ApplicationController
       render :new
     end
   end
-end
 
-private
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
